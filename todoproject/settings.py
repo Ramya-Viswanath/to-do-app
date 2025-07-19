9d4d0ed51daf3a198cb87c01ae6d5069e7302bab
Ramya-Viswanath <ramya.viswanath99@gmail.com>
2025-07-19 20:51:37 +0100

    commiting setings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-td%b16)@-l61*_#5-%ndx3k9j7v&jv%rknedcuq@dh#k!@%_&j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'to-do-app-lv5a.onrender.com']
 
 
 # Application definition
